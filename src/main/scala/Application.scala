1d856166329857ef719ca1ff4eed9502cedd667b
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2015-09-02 10:29:59 +0300

    Добавлена скачка контента

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,9 +1,13 @@
 package main.scala
 
+import scalaj.http.Http
+
 /**
  * @author Anton Bogushevsky
  */
 object Application extends App {
   println("Hello world!")
-  val parser = SimpleHtmlParser("test")
+  
+  val html = Http("https://mail.ru/").asString
+  val parser = SimpleHtmlParser(html)
 }
